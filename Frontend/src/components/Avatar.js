18f7a5990265b392586b1ddfb777cc5a263c40da
Stefanie Satna <heystef@fb.com>
2021-10-12 15:50:50 +0100

    Small fix on Avatar for unstaged change in previous commit

--- Frontend/src/components/Avatar.js
+++ Frontend/src/components/Avatar.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Avatar({ userId, userData }) {
   const avatarImgSrc = userId ? userData[userId].avatar : "";
-  const str = 
+  const str = userId ? userData[userId].name : ""
 
   return (
     <div>
-      <img src={avatarImgSrc} style={style} alt="" />
+      <img src={avatarImgSrc} style={style} alt={str} />
     </div>
   );
 }
