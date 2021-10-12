f6596eab50184cfff9cf1d8a3785f87bc5ced902
Gabriel Davison <mail@gabrieldavison.me.uk>
2021-10-12 15:41:04 +0100

    Authenticated view now renders a dummy for each project

--- Frontend/src/components/Authenticated.js
+++ Frontend/src/components/Authenticated.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DummyProject from './DummyProject'
+
+const dummyProjects = [{name: "Dummy 1", id: 1}, {name: "Dummy 2", id: 2}, {name: "Dummy 3", id: 3}];
 
 export default function Authenticated({user}) {
+    //
+    const [projectsData, setProjectsData] = useState(undefined)
+    const [view, setView] = useState("allProjects")
+
+    const logout = () => {
+        console.log("logout")
+    }
+    useEffect(() => {
+        // fetch projects here
+        setProjects(dummyProjects)
+
+    }, [])
+    const projects = projectsData ? projectsData.map(project => <div key={project.id}><h3>{project.name}</h3></div>) : "No projects to display"
+
     return(
         <div>
             <div>
-                <p>email: {user.email}</p>
-                <p>id: {user.id}</p>
+                <p>{user.email}</p>
+                <button onClick={logout}>Log Out</button>
             </div>
-            <h1>This is the authenticated route.</h1>
+            {view === 'allProjects' ? 
+                <div>
+                    {projects}
+                </div> :
+                <DummyProject id={view}></DummyProject> // Not sure exactly which props the project component will need
+            }
+
         </div>
     )
 }
