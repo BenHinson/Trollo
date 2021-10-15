62e9815e50e83253bc1404d760d866fbd55d3324
Ben Hinson <benhinson@fb.com>
2021-10-15 11:39:49 +0100

    Added Members listener

--- Frontend/src/components/mainview/MainView.js
+++ Frontend/src/components/mainview/MainView.js
@@ -1,20 +1,17 @@
 import React, { useContext } from "react";
+import {ProjectMembersContext} from '../../ProjectsMembersContext';
 
 import Header from './Header';
 import Board from "./Board";
 
 
 export default function MainView({ columns, projectId, refetchBoard, currProjectId }) {
-  // const [members, updateMembers] = useContext(ProjectMembersContext)
-  console.log(members);
-  console.log("rendering main view", columns);
-  let members = [{"id": 1,"email": "example@example.com","username": "example","avatar": null}]
-
+  const [members, updateMembers] = useContext(ProjectMembersContext)
 
   return (
     <section className="mainView">
-      <Header projectName={'Project'} boardName={'Board'} members={members} />
-        <Board columnData={columns} projectId={projectId} refetchBoard={refetchBoard}/>
+      <Header projectName={'Project'} boardName={'Board'} membersArray={members} />
+      <Board columnData={columns} projectId={projectId} refetchBoard={refetchBoard} />
     </section>
   );
 }
